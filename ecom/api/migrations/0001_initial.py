9eff005fd5044af399f20236c6406f141247a060
vandana <vandana@vandu.com>
2021-10-29 03:05:28 +0530

    Initial commit

---
+++ ecom/api/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+from django.db import migrations
+from api.user.models import CustomUser
+
+class Migration(migrations.Migration):
+    def seed_data(apps, schema_editor):
+        user = CustomUser(name = "vandana", 
+                        email="vandana@vandu.com", 
+                        is_staff = True,
+                        is_superuser = True,
+                        phone = "987654321",
+                        gender = "female"
+                        )
+        user.set_password("12345")
+        user.save()
+
+    dependencies = [
+
+    ]
+
+    operations = [
+        migrations.RunPython(seed_data),
+    ]
---
+++ projfrontend/src/user/Signin.js
@@ -0,0 +1,143 @@
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+
+import Base from "../core/Base";
+import { signin, isAuthenticated, authenticate } from "../auth/helper";
+
+const Signin = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false,
+    loading: false,
+    didRedirect: false,
+  });
+
+  const { name, email, password, error, success, loading, didRedirect } =
+    values;
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false, loading: true });
+    signin({ email, password })
+      .then((data) => {
+        console.log("DATA", data);
+        if (data.token) {
+          // let sessionToken = data.token;
+          authenticate(data, () => {
+            console.log("TOKEN ADDED");
+            setValues({
+              ...values,
+              didRedirect: true,
+            });
+          });
+        } else {
+          setValues({
+            ...values,
+            loading: false,
+          });
+        }
+      })
+      .catch((e) => console.log(e));
+  };
+
+  const performRedirect = () => {
+    if (isAuthenticated()) {
+      return <Redirect to="/" />;
+    }
+  };
+
+  const loadingMessage = () => {
+    return (
+      loading && (
+        <div className="alert alert-info">
+          <h2>Loading....</h2>
+        </div>
+      )
+    );
+  };
+
+  const successMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-success"
+            style={{ display: success ? "" : "none" }}
+          >
+            New Account Created SuccessFully. Please
+            <Link to="/signin"> Login Now </Link> Login Now.
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const errorMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-danger"
+            style={{ display: error ? "" : "none" }}
+          >
+            Check all Fields again
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const signInForm = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Email</label>
+              <input
+                className="form-control"
+                value={email}
+                onChange={handleChange("email")}
+                type="text"
+                placeholder="abc@xy.com"
+              />
+            </div>
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input
+                className="form-control"
+                value={password}
+                onChange={handleChange("password")}
+                type="password"
+                placeholder="*******"
+              />
+            </div>
+            <button onClick={onSubmit} className="btn btn-success btn-block">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <Base title="Wel-Come Back To Vandana Apparel" description="Signin to our website if you already have an account ">
+    {successMessage()}
+    {errorMessage()}
+      {loadingMessage()}
+      {signInForm()}
+      {/* <p className="text-center">{JSON.stringify(values)}</p> */}
+      {performRedirect()}
+    </Base>
+  );
+};
+
+export default Signin;
